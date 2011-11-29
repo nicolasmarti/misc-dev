e5ef6377484fe487aa41175e33be39984a54c354
Nicolas Marti <nicolas.marti.japon@gmail.com>
2011-11-29 11:29:49 +0900

    just have float type and value as objects ... still needs to plug in defs + parser

--- src/core/primitives/primitive_float.ml
+++ src/core/primitives/primitive_float.ml
@@ -3,19 +3,19 @@ open Libpprinter
 open Libparser
 open Primitive
 
-let float_uuid = get_fresh_uuid ();;
+let float_type_uuid = get_fresh_uuid ();;
 
 class floatTypeObj =
 object (self)
   inherit [term, context, defs] tObj
-  method uuid = float_uuid
+  method uuid = float_type_uuid
   method get_name = "Float"
   method get_type = Type nopos
   method my_self = (self :> (term, context, defs) tObj)
   method pprint = (fun _ -> Verbatim self#get_name)
   method apply = fun defs context args -> raise (Failure "????")
   method equal = fun o ->
-    if o#uuid = float_uuid then
+    if o#uuid = float_type_uuid then
       true
     else
       false	
@@ -26,3 +26,29 @@ let floatType =
     Obj (o#my_self, nopos)
 ;;
 
+let float_value_uuid = get_fresh_uuid ();;
+
+(* The class for the term *)
+class floatTermObj f =
+object (self)
+  inherit [term, context, defs] tObj
+  val floatvalue = f
+  method uuid = float_value_uuid
+  method get_floatvalue = floatvalue
+  method get_name = string_of_float floatvalue
+  method get_type = floatType
+  method equal = (fun o -> 
+    if o#uuid = float_value_uuid then
+      (((Obj.magic o) :> floatTermObj)#get_floatvalue = self#get_floatvalue)
+    else
+      false
+  )
+  method pprint = (fun _ -> Verbatim self#get_name)
+  method apply = fun defs context args -> raise (Failure "????")
+  method my_self = (self :> (term, context, defs) tObj)
+end;;
+
+let create_floatTerm f =
+  let o = new floatTermObj f in
+    Obj (o#my_self, nopos)
+;;
