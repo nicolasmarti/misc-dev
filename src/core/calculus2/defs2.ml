090a284f5fc2f919cd2823e976ee7e5358f72834
nicolas <nicolas@nicolas-VirtualBox.(none)>
2012-02-29 08:33:08 +0900

    starting new implem

---
+++ src/core/calculus2/defs2.ml
@@ -0,0 +1,19 @@
+(*********************************)
+(* Definitions of data structure *)
+(*********************************)
+
+type name = string
+
+module NameMap = Map.Make(
+  struct
+    type t = string
+    let compare x y = compare x y
+  end
+);;
+
+module NameSet = Set.Make(
+  struct
+    type t = string
+    let compare x y = compare x y
+  end
+);;
