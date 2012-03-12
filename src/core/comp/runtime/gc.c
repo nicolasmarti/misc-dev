e38774564e1904080115851259135450cb329e0e
nicolas <nicolas@nicolas-VirtualBox.(none)>
2012-03-12 09:30:39 +0900

    finish allocation on a segment

--- src/core/comp/runtime/gc.c
+++ src/core/comp/runtime/gc.c
@@ -1,6 +1,7 @@
 #include<stdio.h> //printf
 #include<malloc.h> //memalign
 #include<string.h> //memset
+#include<stdbool.h> // bool, true, false
 /*
 
    this is a memeory allocator / garbage collector
@@ -214,6 +215,19 @@ void set_segment_counter(void* segment, uint counter){
   *(uint*)(segment + 3*ptr_size_byte) = counter;
 }
 
+// increment the segment counter
+void inc_segment_counter(void* segment){
+  uint* counter = (uint*)(segment + 3*ptr_size_byte);
+  *counter = *counter + 1;
+}
+
+// decrement the segment counter
+void dec_segment_counter(void* segment){
+  uint* counter = (uint*)(segment + 3*ptr_size_byte);
+  *counter = *counter - 1;
+}
+
+
 // get pointer to the alloc bitmap
 void* get_alloc_bitmap_ptr(void* segment)
 {
@@ -224,6 +238,17 @@ void* get_alloc_bitmap_ptr(void* segment)
 void* get_root_bitmap_ptr(void* segment, uint nb_bulk)
 {
   return (segment + (4 + bitmap_size_elt(nb_bulk))*ptr_size_byte);
+}
+
+// get a pointer to the data
+void* get_bulk_ptr(void* segment, uint nb_bulk)
+{
+  return (segment + (
+		     4 + 
+		     bitmap_size_elt(nb_bulk) * 2
+		     ) * ptr_size_byte
+	  );
+
 }
 
 // from the starting address of a bitmap (given a nb_bulk), return the address of the level l bitmap
@@ -376,13 +401,6 @@ void* blockAddress(void* data_ptr, bm_index index, bm_mask mask, uint bulk_size)
 
 }
 
-// test if the bitptr for level i is set or not
-uint isMarked(void* level_bitmap_ptr, bm_index index, bm_mask mask)
-{
-  return mask & *(uint*)(level_bitmap_ptr + index*ptr_size_byte);
-
-}
-
 // give the size of element of a given level (correspond to the max index)
 bm_index get_level_max_index(uint nb_bulk, uint level){
   
@@ -414,6 +432,12 @@ bm_mask get_level_max_mask(uint nb_bulk, uint level){
   return 1 << ptr_size_bit - (max_bulk_num - nb_bulk);
 }
 
+// test if the bitptr for level i is set or not
+uint isMarked(void* level_bitmap_ptr, bm_index index, bm_mask mask)
+{
+  return mask & *(uint*)(level_bitmap_ptr + index*ptr_size_byte);
+
+}
 
 //return the next mask
 bm_mask nextMask(void* bitmap_ptr, bm_index index, bm_mask mask, uint level, uint nb_bulk)
@@ -431,7 +455,7 @@ bm_mask nextMask(void* bitmap_ptr, bm_index index, bm_mask mask, uint level, uin
     uint is_marked;
     // loop while
     while (mask && // the mask is not 0 and
-	   mask <= max_index_max_mask && // we are lower or equal to the max mask and
+	   mask < max_index_max_mask && // we are lower or equal to the max mask and
 	   (is_marked = isMarked(level_bitmap_ptr, index, mask)) // the current bit is marked
 	   )
       // we increment the mask
@@ -460,18 +484,191 @@ bm_mask nextMask(void* bitmap_ptr, bm_index index, bm_mask mask, uint level, uin
   return mask;
 }
 
-// TODO
+// setBitAnd: set a bit, and update the upper level w.r.t. a bitwise andif all bits are to one
+void setBitAnd(void* bitmap_ptr, bm_index index, bm_mask mask, uint nb_bulk, uint level)
+{
+  // grab the bitmap pointer of the level
+  void* bitmap_level_ptr = get_level_bitmap_ptr(bitmap_ptr, nb_bulk, level);
+  
+  // grab the corresponding bitmap of the index
+  uint bm = *(uint*)(bitmap_level_ptr + index*ptr_size_byte);
 
-// forwardBitPtr
+  // compute and and with the mask and store
+  bm |= mask;
 
-// findNextFreeBlock
+  *(uint*)(bitmap_level_ptr + index*ptr_size_byte) = bm;
 
-// setBit
+  // update upper map if necessary
+  if (level + 1 < max_level(nb_bulk) && bm == ~0)
+    {
+      bm_index i = index;
+      indexToBitPtr(i, &index, &mask);
+      setBitAnd(bitmap_ptr, index, mask, nb_bulk, level+1);
+    }
 
-// tryAlloc 
+  return;
+}
+
+// setBitOr: set a bit, and update the upper level w.r.t. a bitwise andif at least one bit is one
+void setBitOr(void* bitmap_ptr, bm_index index, bm_mask mask, uint nb_bulk, uint level)
+{
+  // grab the bitmap pointer of the level
+  void* bitmap_level_ptr = get_level_bitmap_ptr(bitmap_ptr, nb_bulk, level);
+  
+  // grab the corresponding bitmap of the index
+  uint bm = *(uint*)(bitmap_level_ptr + index*ptr_size_byte);
+
+  // compute and and with the mask and store
+  bm |= mask;
+  *(uint*)(bitmap_level_ptr + index*ptr_size_byte) = bm;
+
+  // update upper map if necessary
+  bm_index i = index;
+  indexToBitPtr(i, &index, &mask);
+  setBitOr(bitmap_ptr, index, mask, nb_bulk, level+1);
+  
+  return;
+}
+
+// forwardBitPtr: forward the bitptr, until it points to a valid unset bit
+void forwardBitPtr(void* bitmap_ptr, bm_index *index, bm_mask *mask, uint nb_bulk, uint level)
+{
+  // we return false, if we are beyond the proper number of level
+  if (level + 1 >= max_level(nb_bulk))
+    {
+      *mask = 0;
+      return;
+    }
+
+  bm_mask next_mask;
+  bm_index next_index;
+
+  // compute the index in the upper level
+  indexToBitPtr(*index, &next_index, &next_mask);
+
+  // look for the next mask
+  next_mask = nextMask(bitmap_ptr, next_index, next_mask, level+1, nb_bulk);
+
+  // in case we fail => try to look at upper lovel
+  if (next_mask == 0)
+    {
+      forwardBitPtr(bitmap_ptr, &next_index, &next_mask, nb_bulk, level+1);
+      if (next_mask == 0)
+	{
+	  *mask = 0;
+	  return;
+	}
+    }
+
+  // we have or info in next_xxx
+  *index = bitPtrToIndex(next_index, next_mask);
+  *mask = nextMask(bitmap_ptr, *index, 1, level, nb_bulk);
+  return;
+}
+
+// findNextFreeBlock: look for the next available free block
+void findNextFreeBlock(void* bitmap_ptr, bm_index *index, bm_mask *mask, uint nb_bulk, uint level)
+{
+  *mask = nextMask(bitmap_ptr, *index, *mask, level, nb_bulk);
+
+  if (*mask == 0)
+    {
+      forwardBitPtr(bitmap_ptr, index, mask, nb_bulk, level);
+      if (*mask == 0)
+	  return;
+    }
+  
+  return;
+}
+
+// getBulkBitPtr: from a bulk address, get the 
+
+// alloc in a segment
+void* allocSegment(void* segment, bm_index *index, bm_mask *mask, uint nb_bulk, uint bulk_size, bool root)
+{
+  // we grab the bitmap pointer
+  void* bitmap_ptr = get_alloc_bitmap_ptr(segment);
+  
+  // check that the bit pointer is valid, else return NULL
+  uint level_max_index = get_level_max_index(nb_bulk, 0);
+  if (*index > level_max_index) return NULL;
+  uint max_index_max_mask = get_level_max_mask(nb_bulk, 0);
+  if (*index >= level_max_index && *mask >= max_index_max_mask) return NULL;  
+
+  // test if the current pointed bit is marked
+  if (isMarked(bitmap_ptr, *index, *mask))
+    {
+      // yes, let's find the next free block
+      findNextFreeBlock(bitmap_ptr, index, mask, nb_bulk, 0);
+      
+      // if there is none, return NULL
+      if (*mask == 0)
+	return NULL;
+    }
+  
+  // grab the data pointer
+  void* data_ptr = get_bulk_ptr(segment, nb_bulk);
+
+  // compute the allocated block index
+  void* block = blockAddress(data_ptr, *index, *mask, bulk_size);
+
+  // update the root bitmap if necessary
+  if (root)
+    {
+      void* root_bitmap_ptr = get_root_bitmap_ptr(segment, nb_bulk);
+      setBitOr(root_bitmap_ptr, *index, *mask, nb_bulk, 0);
+    }
+  // update the alloc bitmap
+  setBitAnd(bitmap_ptr, *index, *mask, nb_bulk, 0);
+
+  // increment the counter
+  inc_segment_counter(segment);
+
+  // increment the bit pointer
+  inc_bitptr(index, mask);
+
+  // we found the next free block, pointed by index/mask
+  return block;
+
+}
+
+//***************************************************************
+const char *byte_to_binary
+(
+    uint x
+)
+{
+    static char b[33];
+    b[0] = '\0';
+
+    uint z;
+    for (z = 1; z > 0; z <<= 1)
+    {
+        strcat(b, ((x & z) == z) ? "1" : "0");
+    }
+
+    return b;
+}
+
+// print bitmap
+void print_bitmap(void* bitmap_ptr, uint nb_bulk)
+{
+  uint level; 
+  for (level = 0; level < max_level(nb_bulk); ++level)
+    {
+      printf("level: %lu\n", level);
+      uint index;
+      void* level_ptr = get_level_bitmap_ptr(bitmap_ptr, nb_bulk, level);
+      for (index = 0; index <= get_level_max_index(nb_bulk, level); ++index)
+	printf("bm[%lu] := %s\n", level, byte_to_binary(*(uint*)(level_ptr + index*ptr_size_byte)));
+
+    }
+  return;
+}
 
 
 //***************************************************************
+
 
 // allocate a new segment
 void* alloc_segment(uint nb_bulk){
@@ -546,6 +743,7 @@ char gc_init(uint n){
 
   printf("(%p, %p)\n", min_segment_start, max_segment_end);
 
+  /*
   print_list(free_segment_start, free_segment_end);
   alloc_segment(nb_bulk);
   alloc_segment(nb_bulk);
@@ -563,8 +761,39 @@ char gc_init(uint n){
   take_segment_start(&free_segment_start, &free_segment_end);
 
   printf("(%p, %p)\n", min_segment_start, max_segment_end);
+  */
 
-  
+
+  void* segment = alloc_segment(nb_bulk);  
+
+  printf("segment: %p <--> %p\n", segment, segment + (1 << segment_size_n));
+
+  void* alloc_ptr = get_alloc_bitmap_ptr(segment);
+
+  printf("alloc_bitmap_ptr = %p\n", alloc_ptr);
+
+  bm_index index = 0;
+  bm_mask mask = 1;
+  void* alloc = (void*)(1);
+  uint count = 0;
+  while (alloc != NULL)
+    {
+      print_bitmap(alloc_ptr, nb_bulk);
+      alloc = allocSegment(segment, &index, &mask, nb_bulk, bulk_size, false);
+      ++count;
+      printf("alloc(%lu/%lu): %p\n", count, nb_bulk, alloc);
+    }
+
+  print_bitmap(get_alloc_bitmap_ptr(segment), nb_bulk);
+
+  print_bitmap(alloc_ptr, nb_bulk);
+  index = 0;
+  mask = 1;
+  alloc = allocSegment(segment, &index, &mask, nb_bulk, bulk_size, false);
+  ++count;
+  printf("alloc(%lu/%lu): %p\n", count, nb_bulk, alloc);
+
+  /*
   uint i;
   for (i = 0; i < 10000; ++i)
     {
@@ -572,6 +801,8 @@ char gc_init(uint n){
 	printf("%lu != %lu\n", cell_div(i, ptr_size_bit), cell_div_ptr_size_bit(i));
 	
     }
+  */
+
   return -1;
 }
 
@@ -580,7 +811,7 @@ char gc_init(uint n){
 #ifdef WITHMAIN
 int main(int argc, char** argv, char** arge)
 {
-  gc_init(25);
+  gc_init(12);
   
   return 0;
 }
